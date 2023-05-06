bfa0d992ce1370c35df9507d4adb86b56a4cb925
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2023-05-06 02:24:45 +0200

    Use my new MCUtils lib to reduce amount of code

--- src/main/java/com/technicjelle/bluemapcustomskinprovider/BlueMapCustomSkinProvider.java
+++ src/main/java/com/technicjelle/bluemapcustomskinprovider/BlueMapCustomSkinProvider.java
@@ -1,27 +1,17 @@
 package com.technicjelle.bluemapcustomskinprovider;
 
+import com.technicjelle.MCUtils;
 import de.bluecolored.bluemap.api.BlueMapAPI;
 import de.bluecolored.bluemap.api.plugin.SkinProvider;
 import org.bstats.bukkit.Metrics;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.jetbrains.annotations.NotNull;
 import com.technicjelle.UpdateChecker;
 
-import javax.annotation.Nullable;
-import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.nio.file.Files;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.function.Consumer;
-import java.util.logging.Level;
 
-@SuppressWarnings("SameParameterValue")
 public final class BlueMapCustomSkinProvider extends JavaPlugin {
 	UpdateChecker updateChecker;
 
@@ -40,16 +30,11 @@ public final class BlueMapCustomSkinProvider extends JavaPlugin {
 	private final Consumer<BlueMapAPI> blueMapOnEnableListener = blueMapAPI -> {
 		updateChecker.logUpdateMessage(getLogger());
 
-		// Setup config
-		if(getDataFolder().mkdirs()) getLogger().info("Created plugin config directory");
-		File configFile = new File(getDataFolder(), "config.yml");
-		if (!configFile.exists()) {
-			try {
-				getLogger().info("Creating config file");
-				Files.copy(Objects.requireNonNull(getResource("config.yml")), configFile.toPath());
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+		//Copy config.yml from jar to config folder
+		try {
+			MCUtils.copyPluginResourceToConfigDir(this, "config.yml", "config.yml", false);
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 
 		//Load config from disk
@@ -62,34 +47,10 @@ public final class BlueMapCustomSkinProvider extends JavaPlugin {
 			String username = getServer().getOfflinePlayer(playerUUID).getName();
 			String localUrl = url.replace("{UUID}", playerUUID.toString()).replace("{USERNAME}", username);
 			getLogger().info("Downloading skin for " + username + " from " + localUrl);
-			BufferedImage img = imageFromURL(localUrl);
+			BufferedImage img = MCUtils.downloadImage(localUrl);
 			return Optional.ofNullable(img);
 		};
 
 		blueMapAPI.getPlugin().setSkinProvider(customSkinProvider);
 	};
-
-
-	/**
-	 * @param url URL of the image
-	 * @return the image, or null if it could not be found
-	 */
-	private @Nullable BufferedImage imageFromURL(@NotNull String url) {
-		BufferedImage result;
-		try {
-			URL imageUrl = new URL(url);
-			try {
-				InputStream in = imageUrl.openStream();
-				result = ImageIO.read(in);
-				in.close();
-			} catch (IOException e) {
-				getLogger().log(Level.SEVERE, "Failed to get the image from " + url, e);
-				return null;
-			}
-		} catch (MalformedURLException e) {
-			getLogger().log(Level.SEVERE, "URL is malformed: " + url, e);
-			return null;
-		}
-		return result;
-	}
 }
