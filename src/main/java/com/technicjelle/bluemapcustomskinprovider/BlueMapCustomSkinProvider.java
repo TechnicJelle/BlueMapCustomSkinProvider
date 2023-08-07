90ca7ef393ce176c4e961ba5436a0e84b0ca35d4
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2023-08-08 01:56:13 +0200

    Refactor to not reassign a variable

--- src/main/java/com/technicjelle/bluemapcustomskinprovider/BlueMapCustomSkinProvider.java
+++ src/main/java/com/technicjelle/bluemapcustomskinprovider/BlueMapCustomSkinProvider.java
@@ -6,7 +6,9 @@ import de.bluecolored.bluemap.api.plugin.SkinProvider;
 import org.bstats.bukkit.Metrics;
 import org.bukkit.plugin.java.JavaPlugin;
 import com.technicjelle.UpdateChecker;
+import org.jetbrains.annotations.NotNull;
 
+import javax.annotation.Nullable;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.Optional;
@@ -45,9 +47,8 @@ public final class BlueMapCustomSkinProvider extends JavaPlugin {
 		String url = getConfig().getString("url");
 
 		SkinProvider customSkinProvider = playerUUID -> {
-			String username = getServer().getOfflinePlayer(playerUUID).getName();
-			if (username == null)
-				username = playerUUID.toString();
+			@Nullable String bukkitName = getServer().getOfflinePlayer(playerUUID).getName();
+			@NotNull String username = bukkitName != null ? bukkitName : playerUUID.toString();
 			String localUrl = url.replace("{UUID}", playerUUID.toString()).replace("{USERNAME}", username);
 			getLogger().info("Downloading skin for " + username + " from " + localUrl);
 			BufferedImage img = MCUtils.downloadImage(localUrl);
